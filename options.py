5ac4a3f01c5238acf4187c66fb387088d1a4d93e
tugapi <kguoj@outlook.com>
2024-01-12 10:28:22 +0800

    Change arguments

--- options.py
+++ options.py
@@ -6,19 +6,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--corpusFile', default='data/000001SH_index.csv')
 
 
-# TODO 常改动参数
-parser.add_argument('--gpu', default=0, type=int) # gpu 卡号
-parser.add_argument('--epochs', default=100, type=int) # 训练轮数
-parser.add_argument('--layers', default=2, type=int) # LSTM层数
-parser.add_argument('--input_size', default=8, type=int) #输入特征的维度
-parser.add_argument('--hidden_size', default=32, type=int) #隐藏层的维度
-parser.add_argument('--lr', default=0.0001, type=float) #learning rate 学习率
-parser.add_argument('--sequence_length', default=5, type=int) # sequence的长度，默认是用前五天的数据来预测下一天的收盘价
+parser.add_argument('--gpu', default=0, type=int)  # gpu 卡号
+parser.add_argument('--epochs', default=100, type=int)  # 训练轮数
+parser.add_argument('--layers', default=2, type=int)  # LSTM层数
+parser.add_argument('--input_size', default=8, type=int)  # 输入特征的维度
+parser.add_argument('--hidden_size', default=32, type=int)  # 隐藏层的维度
+parser.add_argument('--lr', default=0.0001, type=float)  # learning rate 学习率
+parser.add_argument('--beta1', default=0.5, type=float)
+parser.add_argument('--beta2', default=0.999, type=float)
+parser.add_argument('--sequence_length', default=5, type=int)  # sequence的长度，默认是用前五天的数据来预测下一天的收盘价
+parser.add_argument('--prediction_length', default=1, type=int)  # 待预测天数，默认1天
 parser.add_argument('--batch_size', default=64, type=int)
-parser.add_argument('--useGPU', default=False, type=bool) #是否使用GPU
-parser.add_argument('--batch_first', default=True, type=bool) #是否将batch_size放在第一维
+parser.add_argument('--useGPU', default=False, type=bool)  # 是否使用GPU
+parser.add_argument('--batch_first', default=True, type=bool)  # 是否将batch_size放在第一维
 parser.add_argument('--dropout', default=0.1, type=float)
-parser.add_argument('--save_file', default='model/stock.pkl') # 模型保存位置
+parser.add_argument('--save_file', default='model/stock.pth')  # 模型保存位置
 
 
 args = parser.parse_args()
